a5bf4c98470f0ad7268c0fef7ce18641e3c4880b
saitharun018 <65693649+saitharun018@users.noreply.github.com>
2021-01-27 08:36:25 +0530

    package json error

--- src/Components/Update.js
+++ src/Components/Update.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Form from "react-bootstrap/Form";
-import { useHistory,useParams} from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 import Button from "react-bootstrap/Button";
-import Container from 'react-bootstrap/Container'
-import {withRouter} from 'react-router-dom'
+import Container from "react-bootstrap/Container";
+import { withRouter } from "react-router-dom";
 
 const Update = () => {
   let history = useHistory();
@@ -16,8 +16,8 @@ const Update = () => {
     job: "",
   });
 
-  const { first_name,last_name, job } = Employee;
-  const onInputChange = e => {
+  const { first_name, last_name, job } = Employee;
+  const onInputChange = (e) => {
     setEmployee({ ...Employee, [e.target.name]: e.target.value });
   };
 
@@ -25,64 +25,56 @@ const Update = () => {
     loadEmployees();
   }, []);
 
-  const onSubmit = async e => {
+  const onSubmit = async (e) => {
     e.preventDefault();
     await axios.put(`https://reqres.in/api/users/${id}`, Employee);
     history.push("/");
   };
 
   const loadEmployees = async () => {
-    console.log(id)
+    console.log(id);
     const result = await axios.get(`https://reqres.in/api/users/${id}`);
     setEmployee(result.data.data);
- 
   };
-    return (
-      <>
-        <Container>
-          <Form onSubmit={e => onSubmit(e)} >
-            <Form.Group md="4" controlId="formBasicText">
-              <Form.Label>First Name</Form.Label>
-              <Form.Control
-                type="text"
-                name="first_name"
-                value={first_name}
-                onChange={e => onInputChange(e)}
-                
-              />
-            </Form.Group>
-            <Form.Group md="4" controlId="formBasicEmail">
-              <Form.Label>LastName</Form.Label>
-              <Form.Control
-                name="last_name"
-                value={last_name}
-                onChange={e => onInputChange(e)}
-                type="text"
-                
-              />
-            </Form.Group>
-            <Form.Group md="4" controlId="formBasicEmail">
-              <Form.Label>job</Form.Label>
-              <Form.Control
-                type="text"
-                name="job"
-                value={job}
-                onChange={e => onInputChange(e)}
-              />
-            </Form.Group>
+  return (
+    <>
+      <Container>
+        <Form onSubmit={(e) => onSubmit(e)}>
+          <Form.Group md="4" controlId="formBasicText">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control
+              type="text"
+              name="first_name"
+              value={first_name}
+              onChange={(e) => onInputChange(e)}
+            />
+          </Form.Group>
+          <Form.Group md="4" controlId="formBasicEmail">
+            <Form.Label>LastName</Form.Label>
+            <Form.Control
+              name="last_name"
+              value={last_name}
+              onChange={(e) => onInputChange(e)}
+              type="text"
+            />
+          </Form.Group>
+          <Form.Group md="4" controlId="formBasicEmail">
+            <Form.Label>job</Form.Label>
+            <Form.Control
+              type="text"
+              name="job"
+              value={job}
+              onChange={(e) => onInputChange(e)}
+            />
+          </Form.Group>
 
-            <Button
-              
-              variant="primary"
-              type="submit"
-            >
-              Update
-            </Button>
-          </Form>
-        </Container>
-      </>
-    );
-  }
-
+          <Button variant="primary" type="submit">
+            Update
+          </Button>
+        </Form>
+      </Container>
+    </>
+  );
+};
 
 export default withRouter(Update);
